f251866b603a8d17913deabf841c61c8563e6c91
Miles Thomas <milesman2013@gmail.com>
2023-03-02 16:50:00 +1100

    skeletons added to page files

---
+++ src/pages/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import ProjectCard from '../components/ProjectCard'
+
+const Projects = () => {
+    return (
+        <div className='projects'>
+            
+        </div>
+    )
+}
+
+export default Projects
