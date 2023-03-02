13cccc175b9f4fa34176e083c81a6c725487e460
Miles Thomas <milesman2013@gmail.com>
2023-03-02 16:37:14 +1100

    set up app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Resume from './pages/Resume'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
+        <Navbar />
+        <div className='body'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Projects' element={<Projects />} />
+            <Route path='/Resume' element={<Resume />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
