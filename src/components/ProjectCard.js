604589b510496923b13b89a80367b2706c5eb733
Miles Thomas <milesman2013@gmail.com>
2023-03-02 16:47:17 +1100

    frame made for project cards

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectCard = (props) => {
+    return (
+        <div className='card' key={props.name}>
+            
+        </div>
+    )
+}
+
+export default ProjectCard
