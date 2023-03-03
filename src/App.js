b1486f3b40087b14f7e90f2d3e79fdb93a7d4b52
Miles Thomas <milesman2013@gmail.com>
2023-03-03 17:03:47 +1100

    added footer to app and made file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 import Home from './pages/Home'
 import Projects from './pages/Projects'
@@ -21,6 +22,7 @@ function App() {
             <Route path='/Resume' element={<Resume />} />
           </Routes>
         </div>
+        <Footer />
       </Router>
     </div>
   );
