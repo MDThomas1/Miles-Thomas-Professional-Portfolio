b91f58689f46e6a422d19be8d46b9b478d4e24d9
Miles Thomas <milesman2013@gmail.com>
2023-03-07 13:38:31 +1100

    added base framework to resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Resume = () => {
     return (
         <div className='resume'>
-            
+            <h2>Resume</h2>
+            <div className='resume-content'></div>
+            <p>A downloadable copy of my resume can also be acquired <a href=''>through this link</a></p>
         </div>
     )
 }
