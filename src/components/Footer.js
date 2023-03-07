7639ca631e56506c32f9c292e559222ddf4cdf87
Miles Thomas <milesman2013@gmail.com>
2023-03-07 13:32:50 +1100

    added content and text to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
+            <p>If you wish to reach out to me or see more of my work, you can do so through my <a href='https://www.linkedin.com/in/miles-thomas-125a12261/'>LinkedIn</a> and my <a href='https://github.com/MDThomas1'>GitHub</a></p>
         </div>
     )
 }
