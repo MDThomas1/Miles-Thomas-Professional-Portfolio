82eba4e16f4aa13a7b1ac12ce83ee77f0d0a7bdd
Miles Thomas <milesman2013@gmail.com>
2023-03-07 13:27:47 +1100

    added html tags to home, still need to include text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
     return (
         <div className='home'>
-            
+            <h2 className='page-header'>Welcome to my portfolio!</h2>
+            <img className='profile-image' />
+            <p></p>
         </div>
     )
 }
