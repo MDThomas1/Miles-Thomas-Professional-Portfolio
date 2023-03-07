e3c86bb971c95aec468aeeda470f8de341bd766b
Miles Thomas <milesman2013@gmail.com>
2023-03-07 13:40:49 +1100

    added html content to about me

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <div className='about-me'>
+            <h2 className='page-header'>About Me</h2>
+            <p></p>
+            <p></p>
+        </div>
+    )
+}
+
+export default AboutMe
