bfe94188823e0197ef020c5e4a8b7afbe3de0d59
Miles Thomas <milesman2013@gmail.com>
2023-03-09 14:27:59 +1100

    added html to resume, still need data

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,8 +3,8 @@ import React from 'react'
 const ProjectCard = (props) => {
     return (
         <div className='resume-card' key={props.name}>
-            <h3 className='project-name'>{props.name}</h3>
-            <img src={props.image} alt='Failed to load' />
+            <a href={props.github}><h3 className='project-name'>{props.name}</h3></a>
+            <a href={props.url}><img src={props.image} alt='Failed to load' /></a>
             <p className='project-description'>{props.description}</p>
         </div>
     )
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,44 @@
 import React from 'react'
 
+import ResumeCard from '../components/ResumeCard'
+
 const Resume = () => {
+
+    const experienceData = [
+
+    ]
+
+    const educationData = [
+
+    ]
+
     return (
         <div className='resume'>
             <h2>Resume</h2>
-            <div className='resume-content'></div>
+            <div className='resume-content'>
+                <div className='resume-div'>
+                    <h3>Skills:</h3>
+                    <ul>
+                        <li>HTML | ReactJS | Handlebars</li>
+                        <li>CSS | Bootstrap</li>
+                        <li>JavaScript | NodeJS | Express</li>
+                        <li>MySQL | MongoDB</li>
+                        <li>Git | GitHub | Markdown</li>
+                    </ul>
+                </div>
+                <div className='resume-div'>
+                    <h3>Education</h3>
+                    {educationData.map((education) => {
+                        <ResumeCard name={education.name} body={education.body} />
+                    })}
+                </div>
+                <div className='resume-div'>
+                    <h3>Experience</h3>
+                    {experienceData.map((experience) => {
+                        <ResumeCard name={experience.name} body={experience.body} />
+                    })}
+                </div>
+            </div>
             <p>A downloadable copy of my resume can also be acquired <a href=''>through this link</a></p>
         </div>
     )
