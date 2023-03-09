963a9cae1d21159c6cb79e396937a60032bb637c
Miles Thomas <milesman2013@gmail.com>
2023-03-09 14:30:48 +1100

    added new props to project card

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -43,7 +43,7 @@ const Projects = () => {
             <p>Here you will find a short list of some of the projects that I have made, both on my own or as part of a group. Links to a deployed site as well as a github repository are provided, along with a short description relating to each project.</p>
             <div className='list'>
                 {projectData.map((project) => {
-                    <ProjectCard name={project.name} image={project.image} description={project.description} />
+                    <ProjectCard name={project.name} image={project.image} description={project.description} url={project.url} github={project.github} />
                 })}
             </div>
         </div>
