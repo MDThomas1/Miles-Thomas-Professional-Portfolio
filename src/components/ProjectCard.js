6dc4bacea9613881f99c48290dc9b4539d4efdc0
Miles Thomas <milesman2013@gmail.com>
2023-03-09 14:17:12 +1100

    updated  className on project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProjectCard = (props) => {
     return (
-        <div className='card' key={props.name}>
+        <div className='resume-card' key={props.name}>
             <h3 className='project-name'>{props.name}</h3>
             <img src={props.image} alt='Failed to load' />
             <p className='project-description'>{props.description}</p>
