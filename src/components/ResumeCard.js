089af02679291b35b8c58342e3b4d446602958a0
Miles Thomas <milesman2013@gmail.com>
2023-03-09 14:18:31 +1100

    configured resume card

---
+++ src/components/ResumeCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ResumeCard = (props) => {
+    return (
+        <div className='resume-card' key={props.name}>
+            <h3 className='resume-name'>{props.name}</h3>
+            <p className='resume-body'>{props.description}</p>
+        </div>
+    )
+}
+
+export default ResumeCard
