ee3466f69b077bca68fb0c3ce5abaad39b9fa2cd
Miles Thomas <milesman2013@gmail.com>
2023-03-20 14:32:43 +1100

    new form for resume crad

--- src/components/ResumeCard.js
+++ src/components/ResumeCard.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const ResumeCard = (props) => {
     return (
-        <div className='resume-card' key={props.name}>
-            <h3 className='resume-name'>{props.name}</h3>
-            <p className='resume-body'>{props.body}</p>
+        <div className='resume-card' key={props.heading}>
+            <h3 className='resume-heading'>{props.heading}</h3>
+            <h4 className='resume-subheading'>{props.subheading}</h4>
+            <ul>
+                <li>{props.pointone}</li>
+                <li>{props.pointtwo}</li>
+                <li>{props.pointthree}</li>
+            </ul>
         </div>
     )
 }
