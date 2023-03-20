bba5916973b26ab5793b4416ab1bc56c78d0f8a8
Miles Thomas <milesman2013@gmail.com>
2023-03-20 14:57:51 +1100

    added porjects data without images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,34 +6,46 @@ const Projects = () => {
     
     const projectData = [
         {
-            name: '',
+            name: 'Node README Generator',
             image: '',
-            description: ''
+            description: 'An easy to use command-line application that uses a markdown template to create professional-looking READMEs.',
+            url: 'https://drive.google.com/file/d/1J983OUkVwIzVWd6VdqvW_6bEJa30O7uI/view',
+            github: 'https://github.com/MDThomas1/Miles-Thomas-Node-README-generator'
         },
         {
-            name: '',
+            name: 'Social Media Mongoose Backend',
             image: '',
-            description: ''
+            description: '',
+            url: 'https://drive.google.com/file/d/1E5Uxqcxz7Igb4iprAlt06qs_LE1HqOH9/view',
+            github: 'https://github.com/MDThomas1/Miles-Thomas-Social-Media-Backend'
         },
         {
-            name: '',
+            name: 'Javascript Fundamentals Quiz',
             image: '',
-            description: ''
+            description: 'A brief quiz about some fundamental concepts relating to JavaScript.',
+            url: 'https://mdthomas1.github.io/Miles-Thomas-homework-task-4/',
+            github: 'https://github.com/MDThomas1/Miles-Thomas-homework-task-4'
         },
         {
-            name: '',
+            name: 'Work Day Schedular',
             image: '',
-            description: ''
+            description: 'An interactive daily planner with added display of the current date.',
+            url: 'https://mdthomas1.github.io/Miles-Thomas-homework-task-5/',
+            github: 'https://github.com/MDThomas1/Miles-Thomas-homework-task-5'
         },
         {
-            name: '',
+            name: 'Javascript Password Generator',
             image: '',
-            description: ''
+            description: 'A password generator that lets you specify the password length and the types of characters you want to include in it.',
+            url: 'https://mdthomas1.github.io/Miles-Thomas-homework-task-3/',
+            github: 'https://github.com/MDThomas1/Miles-Thomas-homework-task-3'
         },
         {
-            name: '',
+            name: 'Weather Dashboard',
             image: '',
-            description: ''
+            description: 'A weather tracker application that uses Open Weather API to display the current weather of a searched city.',
+            url: 'https://mdthomas1.github.io/Miles-Thomas-homework-task-6/',
+            github: 'https://github.com/MDThomas1/Miles-Thomas-homework-task-6'
         }
     ]
 
