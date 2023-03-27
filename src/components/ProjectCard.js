2546ac679c247931826abfcbcb166e14ac6fb8f4
Miles Thomas <milesman2013@gmail.com>
2023-03-27 15:50:30 +1100

    changed classname on projectcard to card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProjectCard = (props) => {
     return (
-        <div className='resume-card' key={props.name}>
+        <div className='card' key={props.name}>
             <a href={props.url}><img src={props.image} alt='Failed to load' /></a>
             <a href={props.github}><h3 className='project-name'>{props.name}</h3></a>
             <p className='project-description'>{props.description}</p>
