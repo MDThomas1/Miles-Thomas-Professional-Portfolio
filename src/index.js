c9bbe808aac19f0fa4abf04377dca6b85cc373e4
Miles Thomas <milesman2013@gmail.com>
2023-03-27 17:15:33 +1100

    bugfix index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-import 'reset.css'
-import 'style.css'
+import './reset.css'
+import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
